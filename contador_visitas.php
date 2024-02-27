bc550b3aa4a64bbc897044eef10c784487c56591
fresneda <fresneda@a>
2024-02-27 10:57:52 +0100

    contador cookies

---
+++ contador_visitas.php
@@ -0,0 +1,16 @@
+<?php
+if (!isset($_COOKIE['visitas'])){
+    setcookie('visor','1',time()+3600*24); 
+}
+else{
+$visitas = (int) $_COOKIE['visitas'];
+$visitas++;
+if($visitas===3){
+    setcookie('visor','1',time()-3600*24); //Duracion
+    unset ($_COOKIE['visitas']);
+    echo "cookie destruida";
+}else{
+    setcookie('visor','1',time()+3600*24); //Duracion
+    echo "Bienvenido por $visitas vez";
+}
+}
