7e4b755651f0eb8cf4b6bfb6d6f9277deb93f0ab
fresneda <fresneda@a>
2024-02-28 13:02:56 +0100

    practica 1 de edu terminada

---
+++ oracle/practica1.sql
@@ -0,0 +1,223 @@
+--crear la tabla CENTROS
+CREATE TABLE CENTROS( 
+ Numce NUMBER(4) NOT NULL, 
+ Nomce VARCHAR2(25) NOT NULL UNIQUE, 
+ Dirce VARCHAR2(25), 
+ CONSTRAINT PK_CENTROS PRIMARY KEY(numce)); 
+ -- Crear la tabla DEPARTAMENTOS
+CREATE TABLE DEPARTAMENTOS( 
+numde NUMBER(3) NOT NULL, 
+numce NUMBER(4), 
+direc NUMBER(3), 
+tidir CHAR(1), 
+presu NUMBER(3,1), 
+depde NUMBER(3), 
+ NOMDE VARCHAR2(20), 
+ CONSTRAINT PK_DEPARTAMENTOS PRIMARY KEY(numde), 
+CONSTRAINT FK1_DEPARTAMENTOS FOREIGN KEY(numce) 
+ REFERENCES CENTROS(numce) 
+ ON DELETE CASCADE);
+ -- add foreign key
+ALTER TABLE DEPARTAMENTOS ADD CONSTRAINT FK2_DEPARTAMENTOS 
+FOREIGN KEY(depde) 
+REFERENCES DEPARTAMENTOS(numde); 
+-- Crear la tabla EMPLEADOS
+CREATE TABLE EMPLEADOS( Numem NUMBER(3) NOT NULL, 
+ Extel NUMBER(3), 
+ Fecna DATE, 
+ Fecin DATE, 
+ Salar NUMBER(5), 
+ Comis NUMBER(3), 
+ Numhi NUMBER(1), 
+ NOMEM VARCHAR2(10), 
+ Numde NUMBER(3), 
+ CONSTRAINT PK_EMPLEADOS PRIMARY KEY(numem), 
+ CONSTRAINT FK1_EMPLEADOS FOREIGN KEY(numde) 
+ REFERENCES DEPARTAMENTOS(numde) 
+ ON DELETE CASCADE);
+ -- insertar valores a centro
+ INSERT INTO CENTROS VALUES(10,'SEDE CENTRAL','C/ATOCHA,820,MADRID'); 
+INSERT INTO CENTROS VALUES(20, 'RELACION CON CLIENTES', 'C/ATOCHA,405,MADRID'); 
+--YA PUEDO INSERTAR LOS DATOS DE DEPARTAMENTOS 
+INSERT INTO DEPARTAMENTOS VALUES(100, 10,260,'P',72,NULL, 'DIRECCIÓN GENERAL'); 
+INSERT INTO DEPARTAMENTOS VALUES(110, 20,180,'P',90,100, 'DIRECC.COMERCIAL'); 
+INSERT INTO DEPARTAMENTOS VALUES(111, 20,180,'F',66,110, 'SECTOR INDUSTRIAL'); 
+INSERT INTO DEPARTAMENTOS VALUES(112, 20,270,'P',54,110, 'SECTOR SERVICIOS'); 
+INSERT INTO DEPARTAMENTOS VALUES(120, 10,150,'F',18,100, 'ORGANIZACIÓN'); 
+INSERT INTO DEPARTAMENTOS VALUES(121, 10,150,'P',12,120, 'PERSONAL'); 
+INSERT INTO DEPARTAMENTOS VALUES(122, 10,350,'P',36,120, 'PROCESO DE DATOS'); 
+INSERT INTO DEPARTAMENTOS VALUES(130, 10,310,'P',12,100, 'FINANZAS');
+-- YA INSERTAMOS EMPLEADOS 
+INSERT INTO EMPLEADOS 
+VALUES(110,350,'10/11/1970','15/02/1985',1800,NULL,3,'CESAR',121); 
+INSERT INTO EMPLEADOS 
+VALUES(120,840,'09/06/1968','01/10/1988',1900,110,1,'MARIO',112); 
+INSERT INTO EMPLEADOS 
+VALUES(130,810,'09/09/1965','01/02/1981',1500,110,2,'LUCIANO',112); 
+INSERT INTO EMPLEADOS 
+VALUES(150,340,'10/08/1972','15/01/1997',2600,NULL,0,'JULIO',121); 
+INSERT INTO EMPLEADOS 
+VALUES(160,740,'09/07/1980','11/11/2005',1800,110,2,'AUREO',111); 
+INSERT INTO EMPLEADOS 
+VALUES(180,508,'18/10/1974','18/03/1996',2800,50,2,'MARCOS',110); 
+INSERT INTO EMPLEADOS 
+VALUES(190,350,'12/05/1972','11/02/1992',1750,NULL,4,'JULIANA',121); 
+INSERT INTO EMPLEADOS 
+VALUES(210,200,'28/09/1970','22/01/1999',1910,NULL,2,'PILAR',100); 
+INSERT INTO EMPLEADOS 
+VALUES(240,760,'26/02/1967','24/02/1989',1700,100,3,'LAVINIA',111); 
+INSERT INTO EMPLEADOS 
+VALUES(250,250,'27/10/1976','01/03/1997',2700,NULL,0,'ADRIANA',100); 
+INSERT INTO EMPLEADOS 
+VALUES(260,220,'03/12/1973','12/07/2001',720,NULL,6,'ANTONIO',100); 
+INSERT INTO EMPLEADOS 
+VALUES(270,800,'21/05/1975','10/09/2003',1910,80,3,'OCTAVIO',112); INSERT INTO
+EMPLEADOS 
+VALUES(280,410,'10/01/1978','08/10/2010',1500,NULL,5,'DOROTEA',130); 
+INSERT INTO EMPLEADOS 
+VALUES(285,620,'25/10/1979','15/02/2011',1910,NULL,0,'OTILIA',122); 
+INSERT INTO EMPLEADOS 
+VALUES(290,910,'30/11/1967','14/02/1988',1790,NULL,3,'GLORIA',120); 
+INSERT INTO EMPLEADOS 
+VALUES(310,480,'21/11/1976','15/01/2001',1950,NULL,0,'AUGUSTO',130); 
+INSERT INTO EMPLEADOS 
+VALUES(320,620,'25/12/1977','05/02/2003',2400,NULL,2,'CORNELIO',122); 
+INSERT INTO EMPLEADOS 
+VALUES(330,850,'19/08/1958','01/03/1980',1700,90,0,'AMELIA',112); 
+INSERT INTO EMPLEADOS 
+VALUES(350,610,'13/04/1979','10/09/1999',2700,NULL,1,'AURELIO',122); 
+INSERT INTO EMPLEADOS 
+VALUES(360,750,'29/10/1978','10/10/1998',1800,100,2,'DORINDA',111); 
+INSERT INTO EMPLEADOS 
+VALUES(370,360,'22/06/1977','20/01/2000',1860,NULL,1,'FABIOLA',121); 
+INSERT INTO EMPLEADOS 
+VALUES(380,880,'30/03/1978','01/01/1999',1100,NULL,0,'MICAELA',112); 
+INSERT INTO EMPLEADOS 
+VALUES(390,500,'19/02/1976','08/10/2010',1290,NULL,1,'CARMEN',110); 
+INSERT INTO EMPLEADOS 
+VALUES(400,780,'18/08/1979','01/11/2011',1150,NULL,0,'LUCRECIA',111); 
+INSERT INTO EMPLEADOS 
+VALUES(410,660,'14/07/1968','13/10/1989',1010,NULL,0,'AZUCENA',122); 
+INSERT INTO EMPLEADOS 
+VALUES(420,450,'22/10/1966','19/11/1988',2400,NULL,0,'CLAUDIA',130); 
+INSERT INTO EMPLEADOS 
+VALUES(430,650,'26/10/1967','19/11/1988',1260,NULL,1,'VALERIANA',122); 
+INSERT INTO EMPLEADOS 
+VALUES(440,760,'26/09/1966','28/02/1986',1260,100,0,'LIVIA',111); 
+INSERT INTO EMPLEADOS 
+VALUES(450,880,'21/10/1966','28/02/1986',1260,100,0,'SABINA',112); 
+INSERT INTO EMPLEADOS 
+VALUES(480,760,'04/04/1965','28/02/1986',1260,100,1,'DIANA',111); 
+INSERT INTO EMPLEADOS 
+VALUES(490,880,'06/06/1964','01/01/1988',1090,100,0,'HORACIO',112); 
+INSERT INTO EMPLEADOS 
+VALUES(500,750,'08/10/1965','01/01/1987',1200,100,0,'HONORIA',111); 
+INSERT INTO EMPLEADOS 
+VALUES(510,550,'04/05/1966','01/11/1986',1200,NULL,1,'ROMULO',110); 
+INSERT INTO EMPLEADOS 
+VALUES(550,780,'10/01/1970','21/01/1998',600,120,0,'SANCHO',111);
+---practica 2-1 
+--ejercicio1:
+Select nomde 
+from DEPARTAMENTOS 
+where tidir='F' 
+order by nomde;
+--ejercicio2:
+select nomem,numem,extel 
+from empleados 
+where numde = 121 
+order by nomem;
+--ejercicio3:
+Select nomem,extel 
+from empleados 
+where numde = 110 
+order by nomem;
+--ejercicio4:
+Select comis,nomem,salar 
+from empleados 
+where numhi = 3 
+order by comis;
+--ejercicio5:
+Select comis,nomem,salar 
+from empleados 
+where numhi = 3 
+and comis  is not null 
+order by comis;
+--ejercicio6:
+Select salar,nomem 
+from empleados 
+where numhi = 0 and salar >1200 and salar<1500 
+order by salar desc;
+--ejercicio7:
+select DISTINCT numde 
+from empleados 
+where salar <1500 
+order by numde;
+--ejercicio8:
+select DISTINCT comis 
+from empleados 
+where numde=110 
+order by comis DESC;
+---practica 2-2
+---ejercicio1:
+select 'DEPARTAMENTO DE ' || nomde 
+from departamentos 
+where presu <30 
+order by nomde;
+--ejercicio2:
+select numde|| '-'||nomde AS "Número-nombre",tidir AS "T" 
+from departamentos 
+where presu <30 order by numde;
+
+--ejercicio3:
+select  nomem,salar*12 as "Salario 2014",salar*12*1.02 as "Salario 2015",salar*12*1.02*1.02 as "Salario 2016" 
+from empleados 
+where numhi >4 
+order by nomem;
+--ejercicio4:
+select nomem 
+from empleados 
+where (numhi*120)>(0.2*salar) 
+order by nomem;
+--ejercicio5:
+select nomem as "Nombre",salar+comis as "SALARIO TOTAL"  
+from empleados 
+where numde =112 
+order by "SALARIO TOTAL" desc;
+--ejercicio6:
+select nomem as "Nombre",salar+comis as "SALARIO TOTAL" 
+ from empleados 
+ where numde =112 
+ order by "SALARIO TOTAL" desc;
+--ejercicio7:
+select nomem as "Nombre",salar+comis || ' €' as "SALARIO TOTAL"  
+from empleados 
+where numde =112 
+order by "SALARIO TOTAL" desc;
+--ejercicio8:
+select nomem as "NOMBRE",salar+ (60*(numhi-3)) as "SALARIO TOTAL" 
+from empleados 
+where numhi >3 
+order by "NOMBRE" asc;
+--ejercicio9:
+select nomem as "NOMBRE",salar+ (60*(numhi-3)) as "SALARIO TOTAL" 
+from empleados 
+WHERE numhi >= (
+    SELECT numhi 
+    FROM Empleados 
+    WHERE nomem = 'JULIANA'
+    ) 
+order by "NOMBRE" asc;
+--ejercicio10:
+select nomem 
+from empleados 
+where salar >= (
+    select salar*1.15 
+    from empleados  
+    where nomem = 'CLAUDIA'
+    )
+    order by nomem asc;
+--ejercicio11:
+select nomde 
+from departamentos 
+where depde is null;
