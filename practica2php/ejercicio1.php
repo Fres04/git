85c7b1b065412bd7ca28757cd03b236121e7fcb1
fresneda <fresneda@a>
2024-03-01 08:40:52 +0100

    ejercicios a realizar

---
+++ practica2php/ejercicio1.php
@@ -0,0 +1,17 @@
+<!-- pagina-1.php -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    <h1>Formulario en Dos Pasos</h1>
+    <form action="ejercicio2.php" method="post">
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" name="nombre" required>
+        <input type="submit" value="Siguiente">
+    </form>
+</body>
+</html>
