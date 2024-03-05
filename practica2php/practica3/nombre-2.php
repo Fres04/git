4beac0ef20fadda14e147b4cae322d13e7808731
fresneda <fresneda@a>
2024-03-05 08:56:58 +0100

    suu

--- practica2php/practica3/nombre-2.php
+++ practica2php/practica3/nombre-2.php
@@ -3,7 +3,7 @@ session_start();
 
 if(isset($_POST['nombre']) && !empty($_POST['nombre'])) {
     $_SESSION['nombre'] = $_POST['nombre'];
-    header('Location: index.php');
+    header('Location: index.html');
 } else {
     echo "Por favor, ingrese su nombre.";
 }
